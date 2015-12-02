1df97572bb80538bbe7f2204f9fd1c3881221427
David Moothen <dmoothen@websitti.fr>
2015-12-02 21:54:23 +0100

    Get data from browser.

--- src/js/navigator-info.js
+++ src/js/navigator-info.js
@@ -1,3 +1,12 @@
 (function(){
 
+    var parser = new UAParser();
+
+    navigator.info = {
+        browser: parser.getBrowser(),
+        engine: parser.getEngine(),
+        os: parser.getOS(),
+        device: parser.getDevice()
+    };
+
 })();
